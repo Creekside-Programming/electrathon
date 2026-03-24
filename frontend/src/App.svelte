0c1427df34e92f9e738f68e15880ed50ea0823af
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2026-03-24 08:14:58 -0400

    frontend: Setup header and basic tab system

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,10 +5,31 @@
 </script>
 
 <main>
-  <h1>Electrathon Dashboard</h1>
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="flex-1">
+      <h1 class="text-lg">Electrathon Dashboard</h1>
+    </div>
 
-  <label for="endpointInput">API Endpoint</label>
-  <input id="endpointInput" bind:value={apiEndpoint} />
+    <div class="flex gap-4">
+      <span class="loading loading-spinner loading-md"></span> <!-- https://daisyui.com/components/status/ ??? -->
+      <!-- <label class="text-sm" for="endpointInput">API Endpoint</label> -->
+      <input class="input" id="endpointInput" bind:value={apiEndpoint} />
+    </div>
+  </div>
+  <div class="m-4"> <!-- content -->
+    <div role="tablist" class="tabs tabs-lift ml-2">  
+      <input type="radio" name="my_tabs_3" class="tab ml-4" aria-label="Battery" checked={true} />
+      <div class="tab-content">
+        <div class="bg-base-100 border border-gray-300 rounded">
+          <Chart width="500px" height="500px" apiEndpoint={apiEndpoint} />
+        </div>
+      </div>
 
-  <Chart width="500px" height="500px" apiEndpoint={apiEndpoint} />
+      <input type="radio" name="my_tabs_3" class="tab" aria-label="TAB 2" />
+      <div class="tab-content bg-base-100 border-base-300 p-6">Tab content 2</div>
+
+      <input type="radio" name="my_tabs_3" class="tab" aria-label="TAB 3" />
+      <div class="tab-content bg-base-100 border-base-300 p-6">Tab content 3</div>
+    </div>
+  </div>
 </main>
