e32e82227a5b90d858cc2d46bd1a9ce00f3e3ac6
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2026-03-28 22:08:31 -0400

    backend: add `ruff` and format `main.py`

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,9 @@
+import asyncio
+import random
 from typing import List, Literal
 
 from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
-import asyncio
-import random
-
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -16,18 +15,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class BatteryStatus(BaseModel):
     voltage: float
     amperage: float
+
+
 class BatteryMessage(BaseModel):
     """Represents an update to battery status"""
 
     type: Literal["battery"]
     data: List[BatteryStatus]
 
+
 @app.get("/are_you_alive")
 def are_you_alive():
     return {"ok": True}
+
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -35,9 +39,14 @@ async def websocket_endpoint(websocket: WebSocket):
     while True:
         message = BatteryMessage(
             type="battery",
-            data=[BatteryStatus(voltage=round(random.uniform(10.0, 14.0), 1), amperage=round(random.uniform(40.0, 60.0), 1))]
+            data=[
+                BatteryStatus(
+                    voltage=round(random.uniform(10.0, 14.0), 1),
+                    amperage=round(random.uniform(40.0, 60.0), 1),
+                )
+            ],
         )
 
         await websocket.send_json(message.model_dump())
 
-        await asyncio.sleep(1) # Simulate delay between updates
+        await asyncio.sleep(1)  # Simulate delay between updates
