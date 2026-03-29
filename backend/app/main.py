f2b8c592a9cfe8f18f4520c6366e36ea9a431349
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2026-03-28 20:30:23 -0400

    backend: Add WS stuff and battery status stuff

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,43 @@
-from fastapi import FastAPI
+from typing import List, Literal
+
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+import asyncio
+import random
+
+from pydantic import BaseModel
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/voltage") #what file thing its stored in ex: google.com/voltage
-def get_voltage():
-    print(3)
-    return {"volts": 3, "amps": 100}
+class BatteryStatus(BaseModel):
+    voltage: float
+    amperage: float
+class BatteryMessage(BaseModel):
+    """Represents an update to battery status"""
+
+    type: Literal["battery"]
+    data: List[BatteryStatus]
+
+@app.get("/are_you_alive")
+def are_you_alive():
+    return {"ok": True}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        message = BatteryMessage(
+            type="battery",
+            data=[{"voltage": round(random.uniform(10.0, 14.0), 1), "amperage": round(random.uniform(40.0, 60.0), 1)}]
+        )
+        
+        await websocket.send_json(message.model_dump())
+
+        await asyncio.sleep(1) # Simulate delay between updates
