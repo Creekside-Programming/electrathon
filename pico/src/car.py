3ba92ccbce2e4a5308307926daf241b45a0d6cc0
FifthTundraG <117035030+FifthTundraG@users.noreply.github.com>
2026-04-02 16:13:37 -0400

    INA228 impl from AI

--- pico/src/car.py
+++ pico/src/car.py
@@ -1,14 +1,18 @@
-from machine import Pin
+from machine import I2C, Pin
 from lib import BatteryStatusPacket
 from rylr896 import RYLR896
+from ina228 import INA228
 from common import network_id
 import time
 
 ADDRESS = 0
 
 lora = RYLR896(1, Pin(8), Pin(9))
-
 lora.init_lora(network_id, ADDRESS)
+
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000)
+
+ina228 = INA228()
 
 while True:
     battery_packet = BatteryStatusPacket(11.9, 23.4, 35.2)
